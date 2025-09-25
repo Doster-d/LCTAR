4000b9de1200f74300c46a1690da9055957c9969
Rinat <oblom.ru@gmail.com>
2025-09-25 22:02:21 +0300

    chore: apply ruff formatting

--- arb/arb/views.py
+++ arb/arb/views.py
@@ -2,11 +2,8 @@ from django.conf import settings
 from django.http import HttpResponse
 
 
-def health_check(request):
+def health_check(request):  # noqa: ARG001
     return HttpResponse(
         settings.HEALTH_MESSAGE,
-        content_type='text/plain',
+        content_type="text/plain",
     )
-
-
-
