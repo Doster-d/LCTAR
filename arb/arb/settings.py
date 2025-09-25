67204d3eeab32b35d7a8ebcece13f583b54d5a20
Rinat <oblom.ru@gmail.com>
2025-09-25 22:53:34 +0300

    chore: docker working i think

--- arb/arb/settings.py
+++ arb/arb/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = "arb.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": config("DB_NAME", default="lctar"),
+        "USER": config("DB_USER", default="lctar"),
+        "PASSWORD": config("DB_PASSWORD", default="lctarpassword"),
+        "HOST": config("DB_HOST", default="localhost"),
+        "PORT": config("DB_PORT", default="3306"),
     }
 }
 
